6359789ed5ac93626f48d41c5e2a6804c15a6683
zzy0_0 <z1339502431@gmail.com>
2018-07-05 10:11:51 +0800

    增加四个注解

---
+++ src/main/java/com/zzjson/concurrency/annoations/NotRecommend.java
@@ -0,0 +1,23 @@
+package com.zzjson.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>****************************************************************************</p>
+ * <p><b>Copyright © 2010-2018 rollBall team All Rights Reserved<b></p>
+ * <ul style="margin:15px;">
+ * <li>Description : 不推荐的写法</li>
+ * <li>Version     : 1.0</li>
+ * <li>Creation    : 2018年07月05日</li>
+ * <li>@author     : zzy0_0</li>
+ * </ul>
+ * <p>****************************************************************************</p>
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+    String value() default "";
+}
